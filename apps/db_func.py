866a22332640665efabc5e0cbcccd55472ac6700
서정민 <seojeongmin@seojeongmin-ui-MacBookPro.local>
2019-07-18 22:36:52 +0900

    API auth working

--- apps/db_func.py
+++ apps/db_func.py
@@ -22,10 +22,10 @@ def select_user_tag(db, user_id):
 #사용자 학과 반환
 def select_user_major(db, user_id):
 	with db.cursor() as cursor:
-		sql = "SELECT tag_id FROM user_tag WHERE user_id=%s;"
+		sql = "SELECT tag.tag_id from tag JOIN user_tag on tag.tag_id=user_tag.tag_id where tag_type=3 and user_id = %s;"
 		cursor.execute(sql, (user_id,))
-		result = cursor.fetchall()
-		return result
+		result = cursor.fetchone()
+		return result['tag_id']
 
 #사용자 컬러 변경
 def change_user_color(db, user_id, color):
