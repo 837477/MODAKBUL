d32af206380f4bcf0e3b5687209047eb6bb266ab
서정민 <seojeongmin@seojeongmin-ui-MacBookPro.local>
2019-08-07 19:43:41 +0900

    modakbul update

--- static/js/scroll_progressbar.js
+++
@@ -1,7 +0,0 @@
-var progressIndicator = '<div id="i-am-progress-indicator"></div>';
-$('body').append(progressIndicator);
-
-$(window).on('scroll', function(){
-    var currentPercentage = ($(window).scrollTop() / ($(document).outerHeight() - $(window).height())) * 100;
-  $('#i-am-progress-indicator').width(currentPercentage+'%');
-});
