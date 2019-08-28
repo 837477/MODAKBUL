f9c144d4d6995ea1d48e15ddefcc11f8eb8e67fc
837477 <37999795+837477@users.noreply.github.com>
2019-08-28 20:47:31 +0900

    Update global_func.py

--- global_func.py
+++ global_func.py
@@ -30,73 +30,75 @@ def schedule_init():
 #공모전/취업 크롤러
 def modakbul_crawler():
 	db = connect(host=DB_IP, user=DB_ID, password=DB_PW, db='modakbul', charset='utf8mb4', cursorclass=cursors.DictCursor)
-
-	crawl_posts = crawl_run.crawl()
 	
-	#크롤러 공모전 글들
-	crawl_competition = []
-	#크롤러 취업 글들
-	crawl_employment = []
+	try:
+		crawl_posts = crawl_run.crawl()
 
-	#크롤러 공모전 타이틀 리스트
-	crawl_competition_title = []
-	#크롤러 취업 글들
-	crawl_employment_title = []
+		#크롤러 공모전 글들
+		crawl_competition = []
+		#크롤러 취업 글들
+		crawl_employment = []
 
-	for post in crawl_posts:
-		#공모전 글
-		if '공모전' in post['tag']:
-			#현재 공모전 제목 리스트에 이미 있는지 확인
-			if post['title'] not in crawl_competition_title:
-				crawl_competition.append(post)
-				crawl_competition_title.append(post['title'])
-		#취업 글
-		else:
-			#현재 취업 제목 리스트에 이미 있는지 확인
-			if post['title'] not in crawl_employment_title:
-				crawl_employment.append(post)
-				crawl_employment_title.append(post['title'])
-	
-	#DB 공모전 글들
-	competition_sql = select_tag_in_posts(['공모전'])
-	db_competition = select_posts_list(db, competition_sql)
-	#DB 취업 글들
-	employment_sql = select_tag_in_posts(['취업'])
-	db_employment = select_posts_list(db, employment_sql)
+		#크롤러 공모전 타이틀 리스트
+		crawl_competition_title = []
+		#크롤러 취업 글들
+		crawl_employment_title = []
 
-	#기간이 남은 공모전 글들의 타이틀만 모아놓은 리스트
-	yet_competition = []
-
-	for competition in db_competition:
-		#디비에 있는 공모전 글중 기간이 지난것을 삭제.
-		if competition['post_date'] < datetime.now():
-			delete_post(db, competition['post_id'])
-		else:
-			yet_competition.append(competition['post_title'])
-
-	#취업은 마감 기한이 없기 때문에 매번 전체 삭제 후 새롭게 크롤링 된 글로 갱신시켜준다.
-	for employment in db_employment:
-		delete_post(db, employment['post_id'])
-
-	#공모전 게시글을 기존의 글과 중복검사 후 넣어준다.
-	for competition in crawl_competition:
-		#기존의 남아있는 공모전 글중에 안겹치는 글만 디비에 새롭게 넣어준다.(제목 비교)
-		if competition['title'] not in yet_competition:
-			if 'img_url' in competition:
-				crawl_insert_post(db, "admin", competition['title'], competition['content'], competition['date'], competition['tag'], competition['url'], competition['img_url'])
+		for post in crawl_posts:
+			#공모전 글
+			if '공모전' in post['tag']:
+				#현재 공모전 제목 리스트에 이미 있는지 확인
+				if post['title'] not in crawl_competition_title:
+					crawl_competition.append(post)
+					crawl_competition_title.append(post['title'])
+			#취업 글
 			else:
-				crawl_insert_post(db, "admin", competition['title'], competition['content'], competition['date'], competition['tag'], competition['url'], None)
+				#현재 취업 제목 리스트에 이미 있는지 확인
+				if post['title'] not in crawl_employment_title:
+					crawl_employment.append(post)
+					crawl_employment_title.append(post['title'])
+
+		#DB 공모전 글들
+		competition_sql = select_tag_in_posts(['공모전'])
+		db_competition = select_posts_list(db, competition_sql)
+		#DB 취업 글들
+		employment_sql = select_tag_in_posts(['취업'])
+		db_employment = select_posts_list(db, employment_sql)
+
+		#기간이 남은 공모전 글들의 타이틀만 모아놓은 리스트
+		yet_competition = []
+
+		for competition in db_competition:
+			#디비에 있는 공모전 글중 기간이 지난것을 삭제.
+			if competition['post_date'] < datetime.now():
+				delete_post(db, competition['post_id'])
+			else:
+				yet_competition.append(competition['post_title'])
+
+		#취업은 마감 기한이 없기 때문에 매번 전체 삭제 후 새롭게 크롤링 된 글로 갱신시켜준다.
+		for employment in db_employment:
+			delete_post(db, employment['post_id'])
+
+		#공모전 게시글을 기존의 글과 중복검사 후 넣어준다.
+		for competition in crawl_competition:
+			#기존의 남아있는 공모전 글중에 안겹치는 글만 디비에 새롭게 넣어준다.(제목 비교)
+			if competition['title'] not in yet_competition:
+				if 'img_url' in competition:
+					crawl_insert_post(db, "admin", competition['title'], competition['content'], competition['date'], competition['tag'], competition['url'], competition['img_url'])
+				else:
+					crawl_insert_post(db, "admin", competition['title'], competition['content'], competition['date'], competition['tag'], competition['url'], None)
 
 
-	#취업 게시글은 이미 다 삭제가 되었기 때문에 새로운 크롤링 글들로 채워준다.
-	for employment in crawl_employment:
-		if 'img_url' in employment:
-			crawl_insert_post(db, "admin", employment['title'], employment['content'], employment['date'], employment['tag'], employment['url'], employment['img_url'])
-		else:
-			crawl_insert_post(db, "admin", employment['title'], employment['content'], employment['date'], employment['tag'], employment['url'], None)
+		#취업 게시글은 이미 다 삭제가 되었기 때문에 새로운 크롤링 글들로 채워준다.
+		for employment in crawl_employment:
+			if 'img_url' in employment:
+				crawl_insert_post(db, "admin", employment['title'], employment['content'], employment['date'], employment['tag'], employment['url'], employment['img_url'])
+			else:
+				crawl_insert_post(db, "admin", employment['title'], employment['content'], employment['date'], employment['tag'], employment['url'], None)
 
-
-	db.close()
+		db.close()
+	except:
+		db.close()
 
 #당일 통계 관리
 def today_analysis():
